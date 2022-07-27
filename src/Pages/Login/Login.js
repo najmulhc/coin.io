124483f4d7fb995b8cbd6d6294f717eaeb6d6172
kridi32 <kridi32@outlook.com>
2022-07-27 18:19:28 +0600

    firebase authentication is complete

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,32 +2,66 @@ import React from "react";
 import BasicBlock from "../../Components/Shared/BasicBlock";
 import { Link } from "react-router-dom";
 import SharedAuth from "../../Components/Shared/SharedAuth";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase.init";
+import { useState } from "react";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+  const register = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(email, password);
+  };
+  if (user) {
+    console.log(user.user);
+  }
+  if (error) {
+    console.log(error.message);
+  }
   return (
     <section className="container mx-auto flex justify-center min-h-screen items-center">
       <BasicBlock>
-        <h2 className="text-2xl pr-4 mb-4 ">Sign In into Coin.io</h2>
-        <form>
-          <input
-            type="email"
-            placeholder="Enter email"
-            class="input  bg-neutral block w-full"
-            required
-          />
-          <input
-            type="password"
-            placeholder="Enter password"
-            class="input bg-neutral  w-full mt-4"
-            required
-          />
-          <div className="my-4 text-right w-full">
-          
-            <Link to="#" className="font-light text-primary  text-right">Forgot Password?</Link>
-          </div>
-          <button className="w-full btn btn-primary capitalize">Sign In</button>
-          <p className="text-center py-4">If you don't have account, you can <Link to='/register' className="text-primary">Register</Link></p>
-        </form>
-        <SharedAuth /> 
+        <div className="min-w-[400px]">
+          {" "}
+          <h2 className="text-2xl pr-4 mb-4 ">Sign In into Coin.io</h2>
+          <form onSubmit={register}>
+            <input
+              type="email"
+              placeholder="Enter email"
+              class="input  bg-neutral block w-full"
+              required
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Enter password"
+              class="input bg-neutral  w-full mt-4"
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <div className="my-4 text-right w-full">
+              <Link to="#" className="font-light text-primary  text-right">
+                Forgot Password?
+              </Link>
+            </div>
+            <button className="w-full btn btn-primary capitalize" type="submit">
+              Sign In
+            </button>
+            <p className="text-center py-4">
+              If you don't have account, you can{" "}
+              <Link to="/register" className="text-primary">
+                Register
+              </Link>
+            </p>
+          </form>
+          <SharedAuth />{" "}
+        </div>
       </BasicBlock>
     </section>
   );
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -2,61 +2,110 @@ import React, { useState } from "react";
 import BasicBlock from "../../Components/Shared/BasicBlock";
 import { Link } from "react-router-dom";
 import SharedAuth from "../../Components/Shared/SharedAuth";
-import { BsFillEyeSlashFill , BsFillEyeFill} from "react-icons/bs";
+import { BsFillEyeSlashFill, BsFillEyeFill } from "react-icons/bs";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { auth } from "../../firebase.init";
 const Register = () => {
   const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [password, setPasssword] = useState("");
   const [confrimPassword, setConfirmPassword] = useState("");
-  const [displayPassword, setDisplayPassword]  = useState(false)
+  const [displayPassword, setDisplayPassword] = useState(false);
+  const [displayConfirmPassword, setDisplayConfirmPasseord] = useState(false); 
+  const [
+    createUserWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useCreateUserWithEmailAndPassword(auth);
+  
+  const register = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(email, password);
+  }
+  if (user) {
+    console.log(user.user)
+  } if (error) {
+    console.log(error.message)
+  }
   return (
     <section className="container mx-auto flex justify-center min-h-screen items-center">
       <BasicBlock>
-        <h2 className="text-2xl pr-4 mb-4 min-w-">Sign In into Coin.io</h2>
-        <form>
-          <input
-            type="email"
-            placeholder="Enter your email"
-            name=""
-            id=""
-            className="input block w-full mb-4 bg-neutral"
-          />
+       <div className="min-w-[400px]"> <h2 className="text-2xl pr-4 mb-4 min-w-">Create an account</h2>
+        <form onSubmit={register}>
+          <div>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              name=""
+              onChange={e => setEmail(e.target.value)}
+              id=""
+              className="input block w-full mb-4 bg-neutral"
+              required
+            />
+          </div>
           <div className="input bg-neutral   focus-within:border-2 focus-within:border-gray-500 flex items-center justify-between  w-full   mb-4 ">
             {" "}
             <input
-              type={displayPassword? 'text':'password'}
-              placeholder="Choose a password"
+              type={displayPassword ? "text" : "password"}
+              placeholder="Choose a Password"
               name=""
+              id=""
+              className="bg-neutral outline-none"
+              onChange={e => {setPasssword(e.target.value)}}
+            />
+            <button
+              className="text-xl py-4   "
+              onClick={(e) => {
+                e.preventDefault();
+                setDisplayPassword(!displayPassword);
+              }}
+            >
+              {displayPassword ? (
+                <BsFillEyeFill />
+              ) : (
+                <BsFillEyeSlashFill />
+              )}
+            </button>{" "}
+          </div>
+          <div className="input bg-neutral   focus-within:border-2 focus-within:border-gray-500 flex items-center justify-between  w-full   mb-4 ">
+            {" "}
+            <input
+              type={displayConfirmPassword ? "text" : "password"}
+              placeholder="Confirm Password"
+              name=""
+              onChange={e => {setConfirmPassword(e.target.value)}}
               id=""
               className=" w-full outline-none   bg-neutral"
             />
-            <button className="text-xl py-4   " onClick={e => {
-              e.preventDefault();
-              setDisplayPassword(!displayPassword)
-            }}>
-              {displayPassword ? (<BsFillEyeFill />) : (<BsFillEyeSlashFill />)}
+            <button
+              className="text-xl py-4   "
+              onClick={(e) => {
+                e.preventDefault();
+                setDisplayConfirmPasseord(!displayConfirmPassword);
+              }}
+            >
+              {displayConfirmPassword ? (
+                <BsFillEyeFill />
+              ) : (
+                <BsFillEyeSlashFill />
+              )}
             </button>{" "}
           </div>
-          <input
-            type="email"
-            placeholder="Confirm Password"
-            name=""
-            id=""
-            className="input bg-neutral w-full mb-4"
-          />
+
           <div className="my-4  text-right w-full">
             <Link to="#" className="font-light text-primary  text-right">
               Forgot Password?
             </Link>
           </div>
-          <button className="w-full btn btn-primary capitalize">Sign In</button>
+          {password === confrimPassword && password !== "" ?   <button className="w-full btn btn-primary capitalize">Sign In</button>:  <button className="w-full btn   capitalize btn-disabled" disabled>Sign In</button> }
           <p className="text-center py-4">
-            If you don't have account, you can{" "}
-            <Link to="/register" className="text-primary">
-              Register
+            Already Have an account? {" "}
+            <Link to="/login" className="text-primary">
+              Login now
             </Link>
           </p>
         </form>
-        <SharedAuth />
+        <SharedAuth /></div>
       </BasicBlock>
     </section>
   );
